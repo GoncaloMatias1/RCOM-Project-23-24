b972a4f43429086367fb4125ebdf0a0c1768680c
Gabriel Sousa <94064235+Gabguiel@users.noreply.github.com>
2023-10-21 17:18:07 -0400

    Updated linklayer.h

--- Projeto1_RC/include/link_layer.h
+++ Projeto1_RC/include/link_layer.h
@@ -11,36 +11,9 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-int open_transmitter(char* serial_port, int baudrate, int timeout, int nRetransmissions);
-int close_transmitter();
-int connect_transmitter();
-int disconnect_transmitter();
-int send_packet(const uint8_t* packet, size_t length);
-
-// ==============================
-// _TRANSMITTER_H_ content ends
-// ==============================
-
-// ============================
-// _EMISSOR_H_ content starts
-// ============================
-
-#include <stdint.h>
-
-int open_receptor(char* serial_port, int baudrate);
-int close_receptor();
-int connect_receptor();
-int disconnect_receptor();
-int receive_packet(uint8_t* packet);
-
-// ==========================
-// _EMISSOR_H_ content ends
-// ==========================
-
-// ==============================
-// _FRAME_UTILS_H_ content starts
-// ==============================
-
+#define MAX_PAYLOAD_SIZE 1000
+#define FALSE 0
+#define TRUE 1
 #define FLAG            0x7E
 #define TX_ADDRESS      0x03
 #define RX_ADDRESS      0x01
@@ -57,37 +30,6 @@ int receive_packet(uint8_t* packet);
 #define STUFFED_DATA_SIZE   (DATA_SIZE * 2 + 2)
 
 typedef enum {
-    START,
-    FLAG_RCV,
-    A_RCV,
-    C_RCV,
-    BCC_OK,
-    STOP,
-} state_t;
-
-extern struct data_holder_s {
-    uint8_t buffer[STUFFED_DATA_SIZE + 5];
-    size_t length;
-} data_holder;
-
-extern struct alarm_config_s {
-    volatile int count;
-    int timeout;
-    int num_retransmissions;
-} alarm_config;
-
-size_t stuff_data(const uint8_t* data, size_t length, uint8_t bcc2, uint8_t* stuffed_data);
-size_t destuff_data(const uint8_t* stuffed_data, size_t length, uint8_t* data, uint8_t* bcc2);
-void build_supervision_frame(int fd, uint8_t address, uint8_t control);
-void build_information_frame(int fd, uint8_t address, uint8_t control, const uint8_t* packet, size_t packet_length);
-int read_supervision_frame(int fd, uint8_t address, uint8_t control, uint8_t* rej_byte);
-int read_information_frame(int fd, uint8_t address, uint8_t control, uint8_t repeated_ctrl);
-
-// ============================
-// _FRAME_UTILS_H_ content ends
-// ============================
-
-typedef enum {
     LlTx,
     LlRx,
 } LinkLayerRole;
@@ -100,13 +42,47 @@ typedef struct {
     int timeout;
 } LinkLayer;
 
-#define MAX_PAYLOAD_SIZE 1000
-#define FALSE 0
-#define TRUE 1
+typedef enum {
+    START,
+    FLAG_RCV,
+    A_RCV,
+    C_RCV,
+    BCC_OK,
+    STOP,
+} state;
+
+extern struct data_holder {
+    uint8_t buffer[STUFFED_DATA_SIZE + 5];
+    size_t length;
+} dh;
+
+extern struct alarm_config_s {
+    volatile int count;
+    int timeout;
+    int num_retransmissions;
+} alarm_config;
 
 int llopen(LinkLayer connectionParameters);
 int llwrite(const unsigned char *buf, int bufSize);
 int llread(unsigned char *packet);
 int llclose(int showStatistics);
+size_t transmission_stuff_data(const uint8_t* data, size_t length, uint8_t bcc2, uint8_t* stuffed_data);
+size_t transmission_destuff_data(const uint8_t* stuffed_data, size_t length, uint8_t* data, uint8_t* bcc2);
+void supervision_frame_aux(int fd, uint8_t address, uint8_t control);
+void buildInformation(int fd, uint8_t address, uint8_t control, const uint8_t* packet, size_t packet_length);
+int readSupervision(int fd, uint8_t address, uint8_t control, uint8_t* rej_byte);
+int readInformation(int fd, uint8_t address, uint8_t control, uint8_t repeated_ctrl);
+int open_receptor(char* serial_port, int baudrate);
+int close_receptor();
+int connect_receptor();
+int disconnect_receptor();
+int receive_packet(uint8_t* packet);
+int open_transmitter(char* serial_port, int baudrate, int timeout, int nRetransmissions);
+int close_transmitter();
+int connect_transmitter();
+int disconnect_transmitter();
+int send_packet(const uint8_t* packet, size_t length);
+
+
 
 #endif // _LINK_LAYER_H_
